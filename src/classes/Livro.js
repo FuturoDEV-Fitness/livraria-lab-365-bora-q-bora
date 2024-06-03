355d3489e797e6ba2d1bd4dd74f41b634d50ab1d
[calexd] <[caledarvs@gmail.com]>
2024-06-03 16:21:08 -0300

    Crud Livros Done - Biblioteca Lab 365

--- src/classes/Livro.js
+++ src/classes/Livro.js
@@ -1,8 +1,65 @@
 class Livro {
+    #codigo = 0
+    #nome = ''
+    #quantidadePages = 0
+    #genero = ''
+    #autor = ''
 
-    constructor() {
-       
+    constructor(nome, quantidadePages, genero, autor) {
+       this.#codigo = crypto.randomUUID()
+       this.#nome = nome
+       this.#quantidadePages = quantidadePages
+       this.#genero = genero
+       this.#autor = autor
     }
+
+
+
+    get getCodigo(){
+        return this.#codigo
+     }
+
+     set setCodigo(codigo){
+        return this.#codigo
+     }
+
+
+     get getNome(){
+        return this.#nome
+     }
+
+     set setNome(nome){
+        return this.#nome
+     }
+
+     get getQuantidadePages() {
+        return this.#quantidadePages
+     }
+
+     set setQuantidadePages(quantidadePages) {
+        return this.#quantidadePages
+     }
+
+
+     get getGenero() {
+        return this.#genero
+     }
+
+     set setGenero(Genero) {
+        return this.#genero
+     }
+
+
+     get getAutor() {
+        return this.#autor
+     }
+     set setAutor(autor) {
+        return this.#autor
+     }
+
+     exibir(){
+        console.log(`nome: ${this.#nome}, quantidade de paginas: ${this.#quantidadePages}, genero: ${this.#genero}, autor: ${this.#autor}, codigo: ${this.#codigo}`)
+     }
 
 }
 
--- src/classes/LivroCrud.js
+++ src/classes/LivroCrud.js
@@ -1,9 +1,104 @@
+const fs = require('fs');
+
+
+
 class LivroCrud {
 
     constructor() {
         this.filePath = './src/files/livros.json';
     }
+    
+    writeBook(book) {
+            const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))                    
+            conteudoAtual.push({
+                    codigo: book.getCodigo,
+                    nome: book.getNome,
+                    quantidadePages: book.getQuantidadePages,
+                    genero: book.getGenero,
+                    autor: book.getAutor
+            })
+            fs.writeFileSync(
+            this.filePath,
+            JSON.stringify(conteudoAtual, null, 2),
+            'utf-8'
+        )
+
+    }
+
+
+    readAllBooks() {
+        const allBoooks = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+        console.log('----------------- ---------------- -----------------')
+        console.log('Nomes de todos os livros cadastrados: ')
+
+        allBoooks.forEach(book => {
+            console.log('*', book.nome)
+        })
+        console.log('----------------- ---------------- -----------------')
+    }
+
+
+    readBook(palavra) {
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))        
+        const livroFind = conteudoAtual.find(book => book.nome === palavra)
+        let bookFindValidate = false
+        if(livroFind) {
+            bookFindValidate = true
+            console.log(livroFind)
+        } else {
+            console.log('livro não encontrado')
+        }
+        return bookFindValidate
+    }
+
+    deleteBook(palavra) {
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))        
+        const bookFilter = conteudoAtual.filter(book => book.nome !== palavra)
+        if (conteudoAtual.length === bookFilter.length){
+            console.log('livro não encontrado')
+        } else {
+            fs.writeFileSync(
+                this.filePath,
+                JSON.stringify(bookFilter, null, 2),
+                'utf-8'
+            )
+            console.log('livro eliminado com sucesso');
+        }
+    }
+
+    editBook(bookName, attribBook, newAttribBook) {
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8')) 
+        let bookFind = false    
+        const bookFilter = conteudoAtual.map(book => {
+            if (book.nome === bookName){
+                bookFind = true
+                if(book.hasOwnProperty(attribBook)) {
+                    book[attribBook] = newAttribBook
+                } else{
+                    bookFind = false
+                    console.log(`campo ${attribBook} não encontrado`)
+                }
+            }
+            return book
+        })
+        if (bookFind === true) {
+            fs.writeFileSync(
+                this.filePath,
+                JSON.stringify(bookFilter, null, 2),
+                'utf-8'
+            )
+            console.log('Livro alterado com sucesso');
+        } else {
+            console.log(`Não existe campo com o nome ${attribBook} no ${bookName}`)
+        }
+    return bookFind
+    }
 
 }
+
+
+   
+// deletar usar filter para remover para alterar array, 
+//salva o novo array 
 
 module.exports = LivroCrud;
